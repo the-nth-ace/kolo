cf4d052a5e94e8779980c8a9e33d512965575cc9
Femi Olatubosun <thefemiolatubosun@gmail.com>
2022-11-14 09:43:45 +0100

    More cors

--- src/web/config/Express.ts
+++ src/web/config/Express.ts
@@ -20,12 +20,19 @@ export class ExpressConfig {
   constructor() {
     this.dbContext = Container.get(DbContext);
     this.app = express();
+    this.app.use(
+      cors({
+        origin: "*",
+      })
+    );
     this.app.use(bodyParser.urlencoded({ extended: false }));
     this.app.use(bodyParser.json());
+
     this.app.use(health.ping());
     this.app.use(helmet());
     this.setUpSwagger();
     this.setUpControllers();
+
     this.app.use(this.clientErrorHandler);
     this.connectDB();
   }
